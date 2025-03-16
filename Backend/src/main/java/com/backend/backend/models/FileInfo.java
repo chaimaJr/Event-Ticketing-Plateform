cfb332dbf22dec26e8b079b90b03afd374d99e8d
Chaima Jerbi <chaima.jerbi2@gmail.com>
2025-03-16 18:50:47 +0100

    [Back] File-Upload files

---
+++ Backend/src/main/java/com/backend/backend/models/FileInfo.java
@@ -0,0 +1,15 @@
+package com.backend.backend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class FileInfo {
+    private String name;
+    private String url;
+
+
+}
---
+++ Backend/src/main/java/com/backend/backend/models/ResponseMessage.java
@@ -0,0 +1,13 @@
+package com.backend.backend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class ResponseMessage {
+    private String message;
+
+}
